9315c62442f3aee23c071f532c2ca391e858faf6
lillalee <aam@email.com>
2023-02-09 20:26:07 +0300

    Added comments to code

--- src/main/java/com/github/olorini/gameProcesses/GameResult.java
+++ src/main/java/com/github/olorini/gameProcesses/GameResult.java
@@ -1,5 +1,13 @@
 package com.github.olorini.gameProcesses;
 
+/**
+ * The {@Code GameResult} class represents enums for result of
+ * the Tic-Tac-Toe game. It's possible create four variants of result
+ * NOT_FINISHED - game continue
+ * DRAW - nobody won or lost
+ * X_WINS - gamer used the symbol 'X' has won
+ * O_WINS - gamer used the symbol 'O' has won
+ */
 public enum GameResult {
 
 	NOT_FINISHED("Game not finished", false),
@@ -15,14 +23,26 @@ public enum GameResult {
 		this.isGameOver = isGameOver;
 	}
 
+	/**
+	 * Checks that result equals 'DRAW'
+	 * @return boolean value
+	 */
 	public boolean isDraw() {
 		return this == DRAW;
 	}
 
+	/**
+	 * Checks that any gamer has won
+	 * @return boolean value
+	 */
 	public boolean isEveryoneWon() {
 		return this == X_WINS || this == O_WINS;
 	}
 
+	/**
+	 * Checks that the gamer is over
+	 * @return boolean value
+	 */
 	public boolean isNotGameOver() {
 		return !isGameOver;
 	}
